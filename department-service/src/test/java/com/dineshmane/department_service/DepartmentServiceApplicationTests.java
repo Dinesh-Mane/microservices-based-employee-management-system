9634a1a338875a8de7143bc7077c7e8c22edcdb3
Dinesh <dinesh30mane@gmail.com>
2026-08-21 02:04:24 +0530

    add department and employee microservices, each with its own Mysql db

---
+++ department-service/src/test/java/com/dineshmane/department_service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dineshmane.department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ employee-service/src/main/java/com/dineshmane/employee_service/EmployeeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dineshmane.employee_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+		System.out.println("From Employee Service..!!!");
+	}
+
+}
