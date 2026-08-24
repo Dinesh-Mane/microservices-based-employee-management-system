612e606ca5e013eb0f52db91c106d25b239b4a80
Dinesh <dinesh30mane@gmail.com>
2026-08-24 23:01:38 +0530

    add a new organization service and configure mysql db connection

---
+++ organization-service/src/main/java/com/dineshmane/organization_service/OrganizationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dineshmane.organization_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationServiceApplication.class, args);
+		System.out.println("Hello Dinesh.. from Organization service..");
+	}
+
+}
